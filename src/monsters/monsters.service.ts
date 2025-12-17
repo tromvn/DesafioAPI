b04470e67a63d9958a1c91549f3848a25568acd3
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 16:27:16 -0300

    feat: validar formato de ID

--- src/monsters/monsters.service.ts
+++ src/monsters/monsters.service.ts
@@ -1,6 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { Monster } from './schemas/monster.schema';
 
 @Injectable()
@@ -9,13 +13,22 @@ export class MonstersService {
     @InjectModel(Monster.name) private monsterModel: Model<Monster>,
   ) {}
 
+  // para GET /monsters
   async findAll() {
     return await this.monsterModel.find().exec();
   }
 
-  async findOne(id:string) {
+  // para GET /monsters/:id
+  async findOne(id: string) {
+    // validar id
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException(`Formato de ID inválido: ${id}`);
+    }
+
+    // buscar monstruo
     const monster = await this.monsterModel.findById(id).exec();
 
+    // si no existe, 404
     if (!Monster) {
       throw new NotFoundException(`No se encuentra el monstruo con ID ${id}`);
     }
