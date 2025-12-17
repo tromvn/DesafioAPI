a90031065b493c0753cff1f6499b90cde3a64e51
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 15:44:09 -0300

    feat: implementar método findOne en monsters.service.ts

--- src/monsters/monsters.service.ts
+++ src/monsters/monsters.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Monster } from './schemas/monster.schema';
@@ -11,5 +11,15 @@ export class MonstersService {
 
   async findAll() {
     return await this.monsterModel.find().exec();
+  }
+
+  async findOne(id:string) {
+    const monster = await this.monsterModel.findById(id).exec();
+
+    if (!Monster) {
+      throw new NotFoundException(`No se encuentra el monstruo con ID ${id}`);
+    }
+
+    return monster;
   }
 }
