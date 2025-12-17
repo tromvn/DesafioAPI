61a79d71bd5227efcb4811a140a5f6fa4f77aa13
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 01:32:41 -0300

    feat: nest generate module/controller/service monsters

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MonstersModule } from './monsters/monsters.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI as string),
+    MonstersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/monsters/monsters.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('monsters')
+export class MonstersController {}
