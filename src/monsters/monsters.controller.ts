9e1fead49ed2faf0f3706bd707f10daaf69e6160
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 02:09:07 -0300

    feat: agregar endpoint GET /monsters

--- src/monsters/monsters.controller.ts
+++ src/monsters/monsters.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { MonstersService } from './monsters.service';
 
 @Controller('monsters')
-export class MonstersController {}
+export class MonstersController {
+  constructor(private readonly monstersService: MonstersService) {}
+
+  @Get()
+  findAll() {
+    return this.monstersService.findAll();
+  }
+}
