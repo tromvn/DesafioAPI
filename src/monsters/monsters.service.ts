b0a0151cce23d1642d1c50e940375a407396fa4a
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 02:03:36 -0300

    feat:implementar método findAll en monsters.service.ts

--- src/monsters/monsters.service.ts
+++ src/monsters/monsters.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Monster } from './schemas/monster.schema';
 
 @Injectable()
-export class MonstersService {}
+export class MonstersService {
+  constructor(
+    @InjectModel(Monster.name) private monsterModel: Model<Monster>,
+  ) {}
+
+  async findAll() {
+    return await this.monsterModel.find().exec();
+  }
+}
