df2bc28300d6e8850c1b1f63b69a5fc684382edd
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 01:18:58 -0300

    feat: configurar conexión de MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
