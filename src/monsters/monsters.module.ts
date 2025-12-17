f1104c1ee88ce1b1db2b7860e2f8161f1d5fc7e4
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 01:47:02 -0300

    feat: registrar schema Monster en monsters.module.ts

--- src/monsters/monsters.module.ts
+++ src/monsters/monsters.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MonstersController } from './monsters.controller';
 import { MonstersService } from './monsters.service';
+import { Monster, MonsterSchema } from './schemas/monster.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Monster.name, schema: MonsterSchema }]),
+  ],
   controllers: [MonstersController],
-  providers: [MonstersService]
+  providers: [MonstersService],
 })
 export class MonstersModule {}
