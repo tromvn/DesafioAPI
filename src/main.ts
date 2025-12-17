eded93e41100b5c86d2858db96a419b0c4a1540f
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 17:23:40 -0300

    feat: habilitar globalmente validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // habilitar validación global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina propiedades que no están en DTO
+      forbidNonWhitelisted: true, // lanza error si hay propiedades extra
+      transform: true, // transforma los tipos automáticamente
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
