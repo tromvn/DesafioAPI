bfb008e2c379ec4488d58150fdc8aea2aab40831
tromvn <tomas.bazan.l@gmail.com>
2025-12-17 21:07:05 -0300

    feat: agregar endpoint PUT /monsters/:id

--- src/monsters/monsters.controller.ts
+++ src/monsters/monsters.controller.ts
@@ -1,23 +1,32 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
 import { MonstersService } from './monsters.service';
 import { CreateMonsterDto } from './dto/create-monster.dto';
+import { UpdateMonsterDto } from './dto/update-monster.dto';
 
 @Controller('monsters')
 export class MonstersController {
   constructor(private readonly monstersService: MonstersService) {}
-
+  // GET /monsters
   @Get()
   findAll() {
     return this.monstersService.findAll();
   }
 
+  // GET /monsters/:id
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.monstersService.findOne(id);
   }
 
+  // POST /monsters/
   @Post()
   create(@Body() createMonsterDto: CreateMonsterDto) {
     return this.monstersService.create(createMonsterDto);
+  }
+
+  // PUT /monsters/:id
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateMonsterDto: UpdateMonsterDto) {
+    return this.monstersService.update(id, updateMonsterDto);
   }
 }
